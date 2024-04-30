d06a9600a182f3f8f025dafe3f389abc193da5e6
jkdcode <jk.didriksen@gmail.com>
2024-04-30 15:13:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,23 @@ export default function App() {
         >
           Julie Didriksen
         </a>
-        . It is open-sourced on GitHub and hosted on Netlify.
+        . It is open-sourced on{" "}
+        <a
+          href="https://github.com/jkdcode/react-dictionary"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://julies-dictionary.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
       </footer>
     </div>
   );
