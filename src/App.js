f23bdf5daa106e854747618e6bb79156674332c8
jkdcode <jk.didriksen@gmail.com>
2024-05-01 22:39:13 +0200

    started work on search engine

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>A dictionary will show up here. Eventually.</p>
-      </header>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://julie-makes-stuff.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Julie Didriksen
-        </a>
-        . It is open-sourced on{" "}
-        <a
-          href="https://github.com/jkdcode/react-dictionary"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://julies-dictionary.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <header className="App-header text-center">
+          <h1>Dictionary</h1>
+        </header>
+        <main className="text-center">
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          This project was coded by{" "}
+          <a
+            href="https://julie-makes-stuff.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julie Didriksen
+          </a>
+          . It is open-sourced on{" "}
+          <a
+            href="https://github.com/jkdcode/react-dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://julies-dictionary.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
