31a939adf2ed770a30ed131fb745ab00e1d25f1f
jkdcode <jk.didriksen@gmail.com>
2024-05-09 20:17:25 +0200

    updated h1 font

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header text-center">
-          <h1>ENGLISH DICTIONARY</h1>
+          <h1>English Dictionary</h1>
         </header>
         <main>
           <Dictionary defaultKeyword="hello" />
